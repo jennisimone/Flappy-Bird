5f5508fc9bc366d16c99bf1526da28bef865df24
jenni.foster <jenni.foster@autotrader.co.uk>
2020-08-27 13:03:37 +0100

    first commit

---
+++ Assets/Scripts/BirdFlyComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFlyComponent : MonoBehaviour
+{
+    public GameManager gameManager;
+    public HighScore highScore;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+    public AudioSource audioSource;
+    public AudioClip jumpEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            rb.velocity = Vector2.up * velocity;
+            audioSource.PlayOneShot(jumpEffect);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        gameManager.GameOver();
+        highScore.SetHighscore();
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;
+    public int currentScore = 0;
+
+    private void Start() 
+    {
+        score = 0;    
+    }
+
+    private void Update() 
+    {
+        currentScore = score;
+        GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+    }
+}
